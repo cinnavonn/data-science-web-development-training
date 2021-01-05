4e81264b6f2de0b6afda461fa9a86f336a4a13c2
cinnavonn <76420606+cinnavonn@users.noreply.github.com>
2021-01-04 19:32:42 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 import urllib3.request
-with urllib3.request.urlopen('https://github.com/cinnavonn/training10/blob/main/gapminder_clean.csv') as response:
+with urllib3.request.urlopen('https://github.com/cinnavonn/training11/blob/main/gapminder_clean.csv') as response:
    html = response.read()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = pd.read_csv('https://github.com/cinnavonn/training10/blob/main/gapminder_clean.csv', index_col=[0])
+df = pd.read_csv('https://github.com/cinnavonn/training11/blob/main/gapminder_clean.csv', index_col=[0])
 print(df.replace(r'^\s*$', np.nan, regex=True))
 df.fillna(0, inplace=True)
 
